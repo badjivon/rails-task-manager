d7813a952544d44e8cc7bcabfe87220f8a1f906a
Brice ADJIVON <badjivon@fastmail.com>
2021-11-09 15:11:24 +0100

    terminé

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'tasks', to: 'tasks#index'
+
+  # get 'new', to: 'tasks#new'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # patch 'tasks/:id', to: 'tasks#update'
+  # post 'tasks', to: 'tasks#create'
+
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  resources :tasks
+
 end
